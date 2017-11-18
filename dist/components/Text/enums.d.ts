1a9fc4f43c6e5f28c26287b30de8021ed4d6925b
Travis <travis@travis-ci.org>
2017-11-18 03:10:59 +0000

    Deployed to Github Pages

---
+++ dist/components/Text/enums.d.ts
@@ -0,0 +1,48 @@
+export declare enum TextColor {
+    /**
+     * Primary black text color
+     */
+    PRIMARY = "primary",
+    /**
+     * Secondary dark gray text color
+     */
+    SECONDARY = "secondary",
+    /**
+     * Tertiary color for metadata
+     */
+    METADATA = "metadata",
+    /**
+     * For use on darker backgrounds
+     */
+    WHITE = "white",
+}
+export declare enum TextSize {
+    /**
+     * Large headers. Font-size 2.4rem, line-height 2.8rem.
+     */
+    XXLARGE = "xxLarge",
+    /**
+     * Group headers. Font-size 2.2rem, line-height 2.8rem.
+     */
+    XLARGE = "xLarge",
+    /**
+     * Promo headlines. Font-size 1.8rem, line-height 2.4rem.
+     */
+    LARGE = "large",
+    /**
+     * Body text, default size. Font-size 1.5rem, line-height 2.0rem.
+     */
+    MEDIUM = "medium",
+    /**
+     * Secondary text. Font-size 1.4rem, line-height 2.0rem.
+     */
+    MEDIUM_SUB = "mediumSub",
+    /**
+     * Metadata. Font-size 1.2rem, line-height 1.6rem.
+     */
+    SMALL = "small",
+    /**
+     * Very limited use. Font-size 1.0rem, line-height 1.6rem.
+     */
+    XSMALL = "xSmall",
+}
